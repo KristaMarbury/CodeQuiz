14b55827b49d6d4c27bad5d54941770997e12bbf
Krista Marbury <laetaiustus@gmail.com>
2021-04-23 14:45:34 -0500

    added and altered timer function and added more global variables to fix bugs

--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,35 @@
-//start button reveals quiz
-// let revealQuiz = document.querySelector("#start");
-// revealQuiz.style.display = "block";
-// let answerButton = document.getElementById("");
-
 //variables
 let startButton = document.querySelector("#start");
 let saveScore = document.querySelector("#save-score");
 let questionTitle = document.querySelector("#question-title");
 let optionsDiv = document.querySelector("#options");
-
 let questionIndex = 0;
+let timeEl = document.querySelector("#time");
+let secondsLeft = 100;
+let mainEl = document.querySelector("#main");
+let timerInterval;
 
 // functions //////////////////////////////////
-// start quiz - init
-
-//v function to check the answer v
-// function checkAnswer() {
-//   //if the answer is true, then move to the next question
-//   let answer = this.textContent;
-//   alert(answer);
-// }
-
-//event listner for the question answer
 
 function startQuiz() {
-  // start timer
+  setTime();
 
-  //Create an if statement that states 'when timer is done, move to highscore html
-
-  //v Timer Code v
-  // let timeEl = document.querySelector(".time");
-
-  // // Selects element by id
-  // let mainEl = document.getElementById("main");
-
-  // let secondsLeft = 10;
-
-  // function setTime() {
-  //   // Sets interval in variable
-  //   let timerInterval = setInterval(function () {
-  //     secondsLeft--;
-  //     timeEl.textContent = secondsLeft + " seconds left till its over!";
-
-  //     if (secondsLeft === 0) {
-  //       // Stops execution of action at set interval
-  //       clearInterval(timerInterval);
-  //       // Calls function to create and append image
-  //       sendMessage();
-  //     }
-  //   }, 1000);
-  // }
-
-  //v Function to create and append image v
-
-  // function sendMessage() {
-  //   timeEl.textContent = " ";
-  //   let imgEl = document.createElement("img");
-  //   //picture taken from my bitmoji
-  //   imgEl.setAttribute("src", "./assets/images/hotdiggity.png");
-  //   mainEl.appendChild(imgEl);
-  // }
-
-  // setTime();
-
-  // find dom element to show the question
   getQuestion();
+}
+
+function setTime() {
+  // Sets interval in variable
+  timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till its over!";
+
+    if (secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+  }, 1000);
 }
 
 // get the next question
@@ -74,43 +39,63 @@ function getQuestion() {
   // show the question
   questionTitle.textContent = currentQuestion.text;
   //loop show the choices
+  optionsDiv.textContent = "";
   currentQuestion.options.forEach((option) => {
+    let optionsButton = document.createElement("button");
     optionsButton.textContent = option;
     optionsButton.setAttribute("value", option);
-    optionsButton.onclick = answerCheck;
-    let optionsButton = document.createElement("button");
     optionsDiv.appendChild(optionsButton);
+    optionsButton.onclick = answerCheck;
   });
-  // loop show the choices (buttons)
-  // add event listener for the each button created
 }
 
-// check user selection
 function answerCheck() {
   // check the user selection against correct answer
-  if (optionsButton === questions[questionsIndex].answer) {
+  if (this.value === questions[questionIndex].answer) {
     alert("correct");
-    questionsIndex++;
-    if (questionsIndex < questions.length) {
+    questionIndex++;
+    if (questionIndex < questions.length) {
       getQuestion();
     } else {
       endGame();
     }
-  } //else{}
+  } else {
+    alert("incorrect!");
+  }
 }
+// loop show the choices (buttons)
+// add event listener for the each button created
+
+// check user selection
 
 // end game
 function endGame() {
-  // set their score
-  do keepScore;
-  while (option);
+  clearInterval(timerInterval);
+  timeEl.textContent = " ";
+  let imgEl = document.createElement("img");
+  //picture taken from my bitmoji
+  imgEl.setAttribute("src", "./assets/images/hotdiggity.png");
+  mainEl.appendChild(imgEl);
   // show end screen
-  // clear out timer
+  saveHighScore();
 }
 
 // save high score
 function saveHighScore() {
   // prompt for initials
+  let isSave = prompt("Enter initials to save the game");
+  if (isSave !== "") {
+    let highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+    // format new score object for current user
+    let newScore = {
+      score: secondsLeft,
+      initials: isSave,
+    };
+    // save to localstorage
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+  }
   // save score to localstorage
 }
 
